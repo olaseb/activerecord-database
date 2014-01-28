8bc012c4d0ba6fb9ce125cd94dca70aca2d874e4
ola <ola.seb@hotmail.fr>
2014-01-28 15:14:14 +0100

    Jukebox.sqlite

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -5,7 +5,7 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db,table_name)
-  # your code here
+  rows = db.execute ("SELECT COUNT(*) FROM #{table_name}")
 end
 
 puts "There are #{number_of_rows(db,"Artist")} artists"
@@ -16,10 +16,15 @@ puts "There are #{number_of_rows(db,"MediaType")} media types"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
+artists = db.execute ("SELECT name FROM Artist ORDER BY name")# your code here
+p artists
 
-# returns all the love songs
-love_songs = # your code here
+ 
+# # returns all the love songs
+love_songs = db.execute ("SELECT name FROM Track WHERE name LIKE '%love%'")# your code here
 
-# returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+p love_songs
+# # returns all the tracks that are longer than 5 minutes.
+long_tracks = db.execute ('SELECT name FROM Track WHERE Milliseconds > 600000 ORDER BY Milliseconds') # your code here
+
+p long_tracks
